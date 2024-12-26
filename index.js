8e33779b16a5ca4f08a50a54a19718a78ff13e0c
SSR707 <samandarshavkatov07@gmail.com>
2024-12-27 02:37:25 +0500

    Bot End

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-import { bot } from "./src/bot/index.js";
-import { connectMongodb } from "./src/config/index.js";
+import express from "express";
+import { bot } from "./bot/index.js";
+import { connectMongodb } from "./config/index.js";
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+const DOMAIN = process.env.DOMAIN;
+
+// MongoDB ulanishini chaqiring
 connectMongodb();
-console.log("bot start");
-bot.start();
+
+app.use(express.json());
+app.use(`/${process.env.BOT_TOKEN}`, bot.webhookCallback());
+
+app.get("/", (req, res) => {
+  res.send("Telegram bot ishlayapti!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server ishlayapti: ${DOMAIN || "http://localhost"}:${PORT}`);
+});
