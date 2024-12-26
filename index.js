5b4e220cb857330ca10440d2189991c7863574c1
SSR707 <samandarshavkatov07@gmail.com>
2024-12-27 02:38:43 +0500

    Bot End

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import express from "express";
-import { bot } from "./bot/index.js";
-import { connectMongodb } from "./config/index.js";
+import { bot } from "./src/bot/index.js";
+import { connectMongodb } from "./src/config/index.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 const DOMAIN = process.env.DOMAIN;
 
-// MongoDB ulanishini chaqiring
 connectMongodb();
 
 app.use(express.json());
